1968e670ed4669564fc564b5ead5c2fea7294f28
balwinder4264 <balwindersingh4264@gmail.com>
2022-01-22 14:51:49 -0500

    debug

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -8,14 +8,14 @@ import Cors from 'cors'
 // Initialize the cors middleware
 
 export default async function handler(req, res) {
-  // const collectionref = collection(db, "todos");
-  // const q = query(collectionref, orderBy("timestamp", "desc"));
-  // //Get all the docs
-  // const todos = [];
-  // const querrySnapshot = await getDocs(q);
-  // //itrate through that data
-  // querrySnapshot.forEach(doc => {
-  //   todos.push({ ...doc.data(), id: doc.id, timestamp: doc.data().timestamp.toDate().getTime() })
-  // })
+  const collectionref = collection(db, "todos");
+  const q = query(collectionref, orderBy("timestamp", "desc"));
+  //Get all the docs
+  const todos = [];
+  const querrySnapshot = await getDocs(q);
+  //itrate through that data
+  querrySnapshot.forEach(doc => {
+    todos.push({ ...doc.data(), id: doc.id, timestamp: doc.data().timestamp.toDate().getTime() })
+  })
   res.status(200).json({ name: "todos" })
 }
